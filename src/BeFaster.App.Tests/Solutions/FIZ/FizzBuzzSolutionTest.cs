191cb83bd7bc41ffbb36041b9ccfa93d9f059d7d
sbx_user1060 <sbx_user1060@ip-10-129-60-148.eu-west-2.compute.internal>
2019-01-19 15:51:06 +0000

    Sat Jan 19 15:51:06 UTC 2019

--- src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzSolutionTest.cs
@@ -7,10 +7,41 @@ namespace BeFaster.App.Tests.Solutions.FIZ
     public class FizzBuzzSolutionTest
     {
         [TestCase(3, ExpectedResult = "fizz")]
-        public string TestFizz(int suppliedNumber)
+        public string TestFizzOne(int suppliedNumber)
+        {
+            return FizzBuzzSolution.FizzBuzz(suppliedNumber);
+        }
+
+        [TestCase(6, ExpectedResult = "fizz")]
+        public string TestFizzTwo(int suppliedNumber)
+        {
+            return FizzBuzzSolution.FizzBuzz(suppliedNumber);
+        }
+
+        [TestCase(5, ExpectedResult = "buzz")]
+        public string TestBuzzOne(int suppliedNumber)
+        {
+            return FizzBuzzSolution.FizzBuzz(suppliedNumber);
+        }
+
+        [TestCase(10, ExpectedResult = "buzz")]
+        public string TestBuzzTwo(int suppliedNumber)
+        {
+            return FizzBuzzSolution.FizzBuzz(suppliedNumber);
+        }
+
+        [TestCase(15, ExpectedResult = "fizz buzz")]
+        public string TestFizzBuzz(int suppliedNumber)
+        {
+            return FizzBuzzSolution.FizzBuzz(suppliedNumber);
+        }
+
+        [TestCase(16, ExpectedResult = "16")]
+        public string TestNotFizzNotBuzz(int suppliedNumber)
         {
             return FizzBuzzSolution.FizzBuzz(suppliedNumber);
         }
     }
 }
+
 
