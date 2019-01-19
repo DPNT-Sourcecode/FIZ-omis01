220864fb2ba23d9a2476975e387a1086178daadb
sbx_user1060 <sbx_user1060@ip-10-129-60-148.eu-west-2.compute.internal>
2019-01-19 15:16:09 +0000

    Sat Jan 19 15:16:09 UTC 2019

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -1,4 +1,4 @@
-﻿using BeFaster.App.Solutions.SUM;
+﻿using BeFaster.App.Solutions.HLO;
 using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.HLO
@@ -6,5 +6,11 @@ namespace BeFaster.App.Tests.Solutions.HLO
     [TestFixture]
     public class HelloSolutionTest
     {
+        private const string testMessage = "Hello to the world";
+        [TestCase(testMessage, ExpectedResult = testMessage)]
+        public string TestMessage(string message)
+        {
+            return HelloSolution.Hello(message);
+        }
     }
 }
